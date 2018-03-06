f8d0cb9635e9fa97af5e2fcec8e7621130394ea8
janaleible <jana.leible@web.de>
2018-03-07 00:35:23 +0100

    adapt grid search parameters

--- gridsearch.sh
+++ gridsearch.sh
@@ -5,31 +5,26 @@ if [ -f "output/meta.txt" ] ; then
 fi
 
 python3 script.py "a_example" 1 1 1 &
-python3 script.py "a_example" 10 1 1 &
-python3 script.py "a_example" 1 10 1 &
-python3 script.py "a_example" 1 1 10 &
+python3 script.py "a_example" 1 100 1 &
+python3 script.py "a_example" 1 1 10000 &
 wait
 
 python3 script.py "b_should_be_easy" 1 1 1 &
-python3 script.py "b_should_be_easy" 10 1 1 &
-python3 script.py "b_should_be_easy" 1 10 1 &
-python3 script.py "b_should_be_easy" 1 1 10 &
+python3 script.py "b_should_be_easy" 1 100 1 &
+python3 script.py "b_should_be_easy" 1 1 10000 &
 wait
 
 python3 script.py "c_no_hurry" 1 1 1 &
-python3 script.py "c_no_hurry" 10 1 1 &
-python3 script.py "c_no_hurry" 1 10 1 &
-python3 script.py "c_no_hurry" 1 1 10 &
+python3 script.py "c_no_hurry" 1 100 1 &
+python3 script.py "c_no_hurry" 1 1 10000 &
 wait
 
 python3 script.py "d_metropolis" 1 1 1 &
-python3 script.py "d_metropolis" 10 1 1 &
-python3 script.py "d_metropolis" 1 10 1 &
-python3 script.py "d_metropolis" 1 1 10 &
+python3 script.py "d_metropolis" 1 100 1 &
+python3 script.py "d_metropolis" 1 1 10000 &
 wait
 
 python3 script.py "e_high_bonus" 1 1 1 &
-python3 script.py "e_high_bonus" 10 1 1 &
-python3 script.py "e_high_bonus" 1 10 1 &
-python3 script.py "e_high_bonus" 1 1 10 &
+python3 script.py "e_high_bonus" 1 100 1 &
+python3 script.py "e_high_bonus" 1 1 10000 &
 wait
